f4b185ec26dea18b042067ed258df62cdc40198a
alstjrwjd99 <42230162+alstjrwjd99@users.noreply.github.com>
2023-03-11 23:41:12 +0900

    Delete admin.py

--- admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, Cloth
-
-# Register your models here.
-# User 모델을 등록해줘야함
-admin.site.register(User, UserAdmin)
-admin.site.register(Cloth) #Cloth 모델 등록, admin 사이트에 반영
-UserAdmin.fieldsets += (("Custom fields", {"fields": ("nickname",)}),)
